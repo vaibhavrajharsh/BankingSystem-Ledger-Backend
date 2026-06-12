5c0a29592d94d486623f437674a70b621a188d8b
Vaibhav Raj Harsh <156331614+vaibhavrajharsh@users.noreply.github.com>
2026-06-12 15:08:04 +0530

    security: password hashing using bcryptJS

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,25 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-    email:{
-        type: String,
-        required: [true,"Email is required"],
-        trim: true,
-        lowercase: true,
-        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/,"Invalid email address"],
-        unique:[true,"Email already exists"],
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/,
+        "Invalid email address",
+      ],
+      unique: [true, "Email already exists"],
     },
-    name:{
-        type: String,
-        required:[true,"Name is required"],
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password should be at least 6 characters"],
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-    },
-    password:{
-        type: String,
-        required:[true,"Password is required"],
-        minlength:[6,"Password should be at least 6 characters"],
-        select: false,
-    },
-},{
-    timestamps:true,
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const has = await bcrypt.hash(this.password, 10);
+  return next();
+});
+
+userSchema.method.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel;
